cde69caeffe3b0e4d2a01ffd75a799dca1bc095a
vsharma <vaishalisharmaukl@gmail.com>
2025-12-08 15:57:06 +0530

    addeventlistener on button

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+let inputUnit = document.getElementById("input-unit")
+const convertBtn = document.getElementById("convert-btn")
+
+let lengthUnits = document.getElementById("length-units")
+let volumeUnits = document.getElementById("volume-units")
+let massUnits = document.getElementById("mass-units")
+const inputValue = Number(inputUnit.value);
+convertBtn.addEventListener("click", function(){
+    console.log(typeof inputValue + inputValue);
+    console.log(lengthUnits.textContent);
+    console.log(volumeUnits.textContent);
+    console.log(massUnits.textContent);
+    
+}
+)
 
