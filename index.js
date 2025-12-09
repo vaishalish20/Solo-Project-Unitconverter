ebc64e2e0ae9b3e6f93dd8551c74bc84815bbc03
vsharma <vaishalisharmaukl@gmail.com>
2025-12-09 22:04:19 +0530

    changing textcontents of all p

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-let inputUnit = document.getElementById("input-unit").value
+let inputUnit = document.getElementById("input-unit")
 const convertBtn = document.getElementById("convert-btn")
 
 let lengthUnits = document.getElementById("length-units")
 let volumeUnits = document.getElementById("volume-units")
 let massUnits = document.getElementById("mass-units")
-// let x = 20.987326476
-// console.log(x.toFixed(3));
 
-// const inputValue = Number(inputUnit.value);
 convertBtn.addEventListener("click", function(){
-
-    console.log(`input value is ${inputUnit}`);
-    
+    const userValue = document.getElementById("input-unit").value;
+    console.log(`input value is ${userValue}`);
+    lengthUnits.textContent = `${userValue} meters = ${userValue} feet | ${userValue} feet = 6.096 meters`;
+    volumeUnits.textContent = `${userValue} meters = 65.616 feet | ${userValue} feet = 6.096 meters`;
+    massUnits.textContent = `${userValue} meters = 65.616 feet | ${userValue} feet = 6.096 meters`;
     // console.log(lengthUnits.textContent);
     // console.log(volumeUnits.textContent);
     // console.log(massUnits.textContent);
-    
+    // 20 meters = 65.616 feet | 20 feet = 6.096 meters
+// 20 liters = 5.284 gallons | 20 gallons = 75.708 liters
+// 20 kilograms = 44.092 pounds | 20 pounds = 9.072 kilos
+ 
 }
 )
 
-// let string = `${inputValue} meters = 65.616 feet | ${inputValue} feet = 6.096 meters`
-// console.log(string);
