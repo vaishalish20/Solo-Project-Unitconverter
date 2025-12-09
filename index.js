0303aaba0047105138a12399ba452aa7a420d5fe
vsharma <vaishalisharmaukl@gmail.com>
2025-12-09 21:54:15 +0530

    trying to make changes

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-let inputUnit = document.getElementById("input-unit")
+let inputUnit = document.getElementById("input-unit").value
 const convertBtn = document.getElementById("convert-btn")
 
 let lengthUnits = document.getElementById("length-units")
 let volumeUnits = document.getElementById("volume-units")
 let massUnits = document.getElementById("mass-units")
-const inputValue = Number(inputUnit.value);
+// let x = 20.987326476
+// console.log(x.toFixed(3));
+
+// const inputValue = Number(inputUnit.value);
 convertBtn.addEventListener("click", function(){
-    console.log(typeof inputValue + inputValue);
-    console.log(lengthUnits.textContent);
-    console.log(volumeUnits.textContent);
-    console.log(massUnits.textContent);
+
+    console.log(`input value is ${inputUnit}`);
+    
+    // console.log(lengthUnits.textContent);
+    // console.log(volumeUnits.textContent);
+    // console.log(massUnits.textContent);
     
 }
 )
 
+// let string = `${inputValue} meters = 65.616 feet | ${inputValue} feet = 6.096 meters`
+// console.log(string);
