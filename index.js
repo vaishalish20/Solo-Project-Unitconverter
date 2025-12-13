a25076ef13668715a0c380d20422b50e3111e561
vsharma <vaishalisharmaukl@gmail.com>
2025-12-13 22:44:36 +0530

    trying

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let inputUnit = document.getElementById("input-unit")
+// let inputUnit = document.getElementById("input-unit")
 const convertBtn = document.getElementById("convert-btn")
 
 let lengthUnits = document.getElementById("length-units")
@@ -6,84 +6,53 @@ let volumeUnits = document.getElementById("volume-units")
 let massUnits = document.getElementById("mass-units")
 
 convertBtn.addEventListener("click", function () {
-    let inputUnit = document.getElementById("input-unit").value;
-    console.log(`input value is ${inputUnit}`);
-    console.log(typeof inputUnit);
-    let userValue = Number(inputUnit)
-    console.log(`input value is ${userValue}`);
-    console.log(typeof userValue);
+    const inputUnit = document.getElementById("input-unit").value;
+    console.log(`input value is ${inputUnit} and is of ${typeof inputUnit}`);
+
+    if (isNaN(inputUnit)) {
+        console.log("true, not a number");
+    } else {
+        console.log("number string");
+        const userValue = Number(inputUnit)
+        console.log(`input value is ${userValue}`);
+        console.log(typeof userValue);
+
+    }
+
+
+
+
 
     // lengthUnits.textContent = `${userValue}*${Number(3.281)} meters = ${userValue} feet | ${userValue} feet = 6.096 meters`;
-
-
-    function convertMtrToFt(num1, num2) {
-        return num1 * num2;
-    }
-    function convertFtToMtr(num1, num2) {
-        return num1 / num2;
-    }
-    // ========================================
-    function convertLtrToGal(num1, num2) {
-        return num1 * num2;
-    }
-    function convertGalToLtr(num1, num2) {
-        return num1 / num2;
-    }
-    //=========================================
-    function convertKgsToPnd(num1, num2) {
-        return num1 * num2;
-    }
-    function convertPndToKgs(num1, num2) {
-        return num1 / num2;
-    }
-
-    let mtf = convertMtrToFt(userValue, 3.28084)
-    console.log(mtf);
-    console.log(Math.floor(mtf * 1000) / 1000);
-    // console.log(mtf.toFixed(3));
-    
-
-    let ftm = convertFtToMtr(userValue, 3.28084)
-    console.log(ftm);
-    console.log(Math.floor(ftm * 1000) / 1000);
-// =================================================================
-    let ltg = convertLtrToGal(userValue, 0.264172)
-    console.log(ltg);
-    console.log(Math.floor(ltg * 1000) / 1000);
-    
-    let gtl = convertGalToLtr(userValue, 0.264172)
-    console.log(gtl);
-    console.log(Math.floor(gtl * 1000) / 1000);
-
-// ===================================================================
-    let ktp = convertKgsToPnd(userValue, 2.20462)
-    console.log(ktp);
-    console.log(Math.floor(ktp * 1000) / 1000);
-    
-    let ptk = convertPndToKgs(userValue, 2.20462)
-    console.log(ptk);
-    console.log(Math.floor(ptk * 1000) / 1000);
-    
-
     // volumeUnits.textContent = `${userValue} meters = 65.616 feet | ${userValue} feet = 6.096 meters`;
     // massUnits.textContent = `${userValue} meters = 65.616 feet | ${userValue} feet = 6.096 meters`;
-    // console.log(lengthUnits.textContent);
-    // console.log(volumeUnits.textContent);
-    // console.log(massUnits.textContent);
-    // 20 meters = 65.616 feet | 20 feet = 6.096 meters
-    // 20 liters = 5.284 gallons | 20 gallons = 75.708 liters
-    // 20 kilograms = 44.092 pounds | 20 pounds = 9.072 kilos
+
+    // 20 meters = 65.616 feet | 
+    // 20 feet = 6.096 meters
+
+    // 20 liters = 5.284 gallons | 
+    // 20 gallons = 75.708 liters
+
+    // 20 kilograms = 44.092 pounds | 
+    // 20 pounds = 9.072 kilos
 
 }
 )
 
-/*steps to solve
+
+
+/*
+
+
+steps to solve:
+
 1. target the input value  --done
+    first check the input value is a valid number, 
 2. convert the text into number --done
 3. multiple it with the given rules 
 
 1 meter = 3.281feet
-1 liter =0.264gallon
+1 liter = 0.264gallon
 1 kilogram = 2.204 pound
 
 1 meter = 3.28084 feet
